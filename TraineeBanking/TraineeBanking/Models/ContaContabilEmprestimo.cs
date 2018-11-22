7205a13b08a640303842f21fec2f9acb4a1cf33b
Rafael Borduchi Amoroso <rafa_borduchi@hotmail.com>
2018-11-22 16:30:13 -0200

    Classe emprestimo

---
+++ TraineeBanking/TraineeBanking/Models/ContaContabilEmprestimo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TraineeBanking.Models
+{
+    public class ContaContabilEmprestimo
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private double saldo;
+
+        public double Saldo
+        {
+            get { return saldo; }
+            set { saldo = value; }
+        }
+
+
+    }
+}
